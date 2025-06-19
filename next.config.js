9daaaa296202a38703fbdc5195b02016e13f7195
facundoinfinure <facundo@infinure.com>
2025-06-19 18:30:35 -0300

    fix: add Google OAuth env variables to next.config.js

--- next.config.js
+++ next.config.js
@@ -168,6 +168,8 @@ const nextConfig = {
   env: {
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET
   },
 
   // Configuración de redirecciones
